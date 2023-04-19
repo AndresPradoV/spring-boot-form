b385ae82a700bab2ea9c173fda3a7daee3ea4771
Andres Prado V <andresprado.goit@gmail.com>
2023-04-19 17:36:11 -0500

    Se agrega un comentario

--- src/main/java/com/bolsadeideas/springboot/form/app/SpringBootFormApplication.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/SpringBootFormApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootFormApplication {
-
+//Se agrega un comentario
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootFormApplication.class, args);
 	}
