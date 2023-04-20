6a520b26a586c2f0b47e7cffd3c6f160636519a3
Andres Prado V <andresprado.goit@gmail.com>
2023-04-20 13:12:04 -0500

    Realizando commit

--- src/main/java/com/bolsadeideas/springboot/form/app/SpringBootFormApplication.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/SpringBootFormApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootFormApplication {
-//Se agrega un comentario
-	//Se agrega otro comentario
-	//Se crea un comentario en la rama Main
-	//Se crea otro comentario en la rama Main
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootFormApplication.class, args);
 	}
