79be86a27b94e852395bb00d9d4a70c356e088dc
Andres Prado V <andresprado.goit@gmail.com>
2023-04-20 13:13:54 -0500

    Se crea un comentario en el Main

--- src/main/java/com/bolsadeideas/springboot/form/app/SpringBootFormApplication.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/SpringBootFormApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootFormApplication {
 
+	//Comentario en el main
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootFormApplication.class, args);
 	}
