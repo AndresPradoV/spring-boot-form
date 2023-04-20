4ed58a51d420e6eeba0bf1e02cc92b7bf7e6b53f
Andres Prado V <andresprado.goit@gmail.com>
2023-04-20 13:36:29 -0500

    Se agrega un comentario

--- src/main/java/com/bolsadeideas/springboot/form/app/models/entity/Usuario.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/models/entity/Usuario.java
@@ -14,7 +14,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 public class Usuario {
 
 	//Se agrega comentario por otro lado
-	//Se agrega un comentario en la rama Main
+	//Se agrega un comentario en la ramaSilva
 	private String identificador;
 	
 	private String nombre;
